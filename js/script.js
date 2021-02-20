eb141d6a282445576e992c7d56234e87031373c4
Abraham Sarache <75433744+abrahamsarache@users.noreply.github.com>
2021-02-20 20:22:37 +0100

    Comments
    Wrote comments for the JS page

--- js/script.js
+++ js/script.js
@@ -3,37 +3,33 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+"showPage" function
+It creates and appends dinamically a serie of HTML elements to display 9 students per page.
+   Parameters
+   -list: Uses any database.
+   -page: Determines the number of the currently selected page. 
 */
 
 function showPage(list, page) {
-
+   //these 2 variables will help assigning 9 students per page.
    const startIndex = (page * 9) - 9;
    const endIndex = (page * 9);
 
+   //select the UL where we want to attach the new elements
    let studentList = document.getElementsByClassName('student-list')[0];
       studentList.innerHTML = '';
 
-   //loop over the list parameter
-
-         for (let i=0; i<list.length; i++) {
+   //loop over the list parameter to obtain all objects contained in 'list'.
+         for (let i=0; i < list.length; i++) {
             
             if (startIndex <= i && endIndex > i) {
+               //create a new li and append all elements needed to display all the information of each student
                const li = document.createElement('li');
                li.className = "student-item cf";
                studentList.appendChild(li);
 
-                  //First Div element
+                  //First Div element: Image, full name, email.
                   const detailsDiv = document.createElement('div');
                   detailsDiv.className = "student-details";
                   li.appendChild(detailsDiv);
@@ -57,8 +53,7 @@ function showPage(list, page) {
                      detailsDiv.appendChild(emailSpan);
 
 
-                  //Second DIV element
-
+                  //Second DIV element: Extra info
                   const joinedDiv = document.createElement('div');
                   joinedDiv.className = "joined-details";
                   li.appendChild(joinedDiv);
@@ -73,17 +68,19 @@ function showPage(list, page) {
 };
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+"addPagination" function
+It creates the elements needed for the pagination buttons
 */
 
 function addPagination(list) {
-  let buttonsNeeded = Math.ceil(data.length/9);
+  //this small formula will help us creating a dynamic amount of buttons depending on the database's length.
+  let buttonsNeeded = Math.ceil(list.length/9);
 
+      //select the UL where we want to attach our buttons.
       let linkList = document.getElementsByClassName('link-list')[0];
       linkList.innerHTML = '';
 
-      for (let i=0; i<buttonsNeeded; i++){
+      for (let i=0; i < buttonsNeeded; i++){
          const li = document.createElement('li');
          linkList.appendChild(li);
 
@@ -92,10 +89,12 @@ function addPagination(list) {
          button.textContent = i+1;
          li.appendChild(button);
 
+         //adds active status to the first button
          const firstButton = document.querySelector('.link-list li button');
          firstButton.className = "active";
       }
 
+      //this event listener will change the active status to a different button if it is clicked
       linkList.addEventListener('click', (e)=>{
          if(e.target.tagName === 'BUTTON') {
             let activeButton = document.querySelector(".active");
@@ -108,6 +107,6 @@ function addPagination(list) {
 };
 
 
-// Call functions
+// Call our functions
 showPage(data, 1);
 addPagination(data);
